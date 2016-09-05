295419666c3573844fa4cebe9e12ab88abd66510
hejie <jie.he@nanguache.com>
2016-09-05 14:32:11 +0800

    build

--- app/src/main/java/com/app/demo/SplashActivity.java
+++ app/src/main/java/com/app/demo/SplashActivity.java
@@ -3,7 +3,6 @@ package com.app.demo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-import com.app.demo.utils.ToastUtils;
 import com.example.keydemo.R;
 
 public class SplashActivity extends FragmentActivity {
@@ -12,9 +11,5 @@ public class SplashActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToastUtils.toast("one");
-        ToastUtils.toast("one");
-        ToastUtils.toast("one");
-
     }
 }
